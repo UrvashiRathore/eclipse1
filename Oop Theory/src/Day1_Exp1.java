d0908d9dfb62b8233b710bed89605a0e08a6aee3
UrvashiRathore <rathore.urvashi21@gmail.com>
2021-09-29 20:50:54 +0530

    try change

--- Oop Theory/src/Day1_Exp1.java
+++ Oop Theory/src/Day1_Exp1.java
@@ -2,8 +2,32 @@
 public class Day1_Exp1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		int n=10;
+		int stu[]=new int[n];
+			stu[0] = 49;
+			stu[1] = 50;
+			stu[2]=57;
+			stu[3]=74;
+			stu[4]=59;
+			stu[5]=72;
+			stu[6]=85;
+			stu[7]=93;
+			stu[8]=79;
+			stu[9]=90;
+			char a = 1,b = 2,c = 3;
+		int result = 0;
+		result = stu[n] > 75 ? a : stu[n] > 50 ? b: c;
+		switch(result) {
+		case'a':
+			System.out.println("the result is merit");
+			break;
+		case'b':
+			System.out.println("the result is pass ");
+			break;
+		case'c':
+			
+			System.out.println("the result is fail");
+			break;
+}
+}
 }
